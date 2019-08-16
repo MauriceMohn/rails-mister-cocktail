a0acba7e415152d3e3e30306b1fce4c64ec041aa
Maurice Mohn <maurice_mohn@msn.com>
2019-08-16 16:44:29 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,24 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "rum")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "tonic water")
-Ingredient.create(name: "bitter lemon")
-Ingredient.create(name: "ginger beer")
+require 'json'
+require 'open-uri'
+
+puts "Destroying everything..."
+puts "Seeding your database one ingredient at a time..."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drink_serialized = open(url).read
+drinks = JSON.parse(drink_serialized)
+# p drink
+drinks.each_with_index do |drink, index|
+  list = drink[1]
+  list.each do |x|
+    Ingredient.create(name: x["strIngredient1"])
+  end
+end
+
+puts "Your database has successfully been populated with #{Ingredient.count} ingredients!"
