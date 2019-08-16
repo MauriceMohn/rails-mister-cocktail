b05d42749edb2e788ed4fc4914aa16c8df1b6f88
Maurice Mohn <maurice_mohn@msn.com>
2019-08-16 12:30:55 +0200

    links btw pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get 'doses/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
-      resources :doses, only: [:new, :destroy, :create]
+      resources :doses, only: [:new, :show, :destroy, :create]
     end
+  root to: 'cocktails#index'
 end
